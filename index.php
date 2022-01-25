47008e740b7262e3d37bfd82f44df9f8bfae1a64
Vladislav Beloglazov <pro100king23@yandex.ru>
2022-01-25 17:45:19 +0300

    lesson 3

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
 
-// Задание 1-2
-$dir_images = scandir('./images'); // сканируем директорию
-$src_dir_img = "./images"; // путь к директории
+//  1. Здесь используется решения, по которому не сможет произойти антипаттерн "Слепая вера", так как в коде проверяем подключение к БД, 
+// и в случае возникновения каких-то трудностей к БД, будем давать ошибку "Connection failed to DB"
 
-$link = mysqli_connect('localhost', 'root', '', 'gb'); // подключаемся к БД
+$dir_images = scandir('./images'); 
+$src_dir_img = "./images"; 
+
+$link = mysqli_connect('localhost', 'root', '', 'gb'); 
 
 if ($link) {
-    foreach ($dir_images as $dir) { // фильтруем директорию на файлы в папке, и добавляем файлы в массив
+    foreach ($dir_images as $dir) { 
         if ($dir != '.' && $dir != "..") {
             $src_images[] = $dir;
         }
     }
-    foreach ($src_images as $src_image) { // сортируем файлы по-очереди
+    foreach ($src_images as $src_image) { 
         $filesize = filesize("$src_dir_img/$src_image");
         $insert_in_db = mysqli_query($link, "INSERT INTO images (src, name, size) VALUES ('$src_dir_img/$src_image', '$src_image', '$filesize');");
     };
@@ -28,3 +30,20 @@ function render($arg){
        echo '<a target="_blank" href="' . $result['src'] . '"><img src="' . $result['src'] . '" with="100px" height="100px"> </img>';
     }
 }
+
+// Пример спаггети-кода, когда переменные abc не несут какой-то информации. В идеале, наименование переменных нужно поменять на более подходящие под смыслы.
+
+<?php
+ /* Принимаем данные из формы */
+  $connect=mysqli_connect('localhost','login','password') or die(mysqli_error());
+mysqli_select_db('myDB') or die ("Не могу выбрать базу данных");
+if(isset($_POST['enter'])){/*если нажата кнопка, то заносим в бд данные*/
+	 if (empty($_POST['name']) or empty($_POST['phone'])) {
+        echo '<script>alert("Заполните все поля формы");</script>';
+	 } else {
+  $a = $_POST["name"];
+  $b = $_POST["phone"];
+  $c = date("l dS of F Y h:i:s A");
+$mysql=mysqli_query($connect,"INSERT INTO contacts VALUES ('','$name','$phone','$datetime','')") OR DIE (MYSQLI_ERROR());}}
+  ?>
+
